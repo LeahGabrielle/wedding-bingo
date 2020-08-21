34c9d6beaa1be586dedd449731bea788d3da3fd9
LeahGabrielle <olexson@ualberta.ca>
2020-08-20 23:49:51 -0600

    grid styles and adding more numbers

--- src/components/BingoSheet.vue
+++ src/components/BingoSheet.vue
@@ -1,7 +1,15 @@
 <template>
    <h1>It's bingo time my dudes</h1>
-   <div v-for="i in 4" :key="i">
-      <BingoTile v-bind:text="pickTileText()"></BingoTile>
+   <div class="sheet">
+      <div class="tile" v-for="i in 12" :key="i">
+         <BingoTile v-bind:text="pickTileText()"></BingoTile>
+      </div>
+      <div class="tile">
+         <BingoTile v-bind:text="'ring_img'"></BingoTile>
+      </div>
+      <div class="tile" v-for="i in 12" :key="i">
+         <BingoTile v-bind:text="pickTileText()"></BingoTile>
+      </div>
    </div>
 </template>
 
@@ -9,7 +17,29 @@
 var allTexts = [
 "1",
 "2",
-"3"
+"3",
+"4",
+"5",
+"6",
+"7",
+"8",
+"9",
+"10",
+"11",
+"12",
+"13",
+"14",
+"15",
+"16",
+"17",
+"18",
+"19",
+"20",
+"21",
+"22",
+"23",
+"24",
+"25",
 ]
 
 import BingoTile from './BingoTile.vue'
@@ -20,7 +50,6 @@ import BingoTile from './BingoTile.vue'
          var randomNum = Math.floor(Math.random() * allTexts.length);
          const randomElement = allTexts[randomNum];
          allTexts.splice(randomNum, 1);
-         //console.log(allTexts);
          return randomElement;
       }
     },
@@ -31,5 +60,11 @@ import BingoTile from './BingoTile.vue'
 </script>
 
 <style>
-
+.sheet{
+   display:grid;
+   grid-template-columns: 1fr 1fr 1fr 1fr 1fr;
+}
+.tile{
+   width: 20%;
+}
 </style>
